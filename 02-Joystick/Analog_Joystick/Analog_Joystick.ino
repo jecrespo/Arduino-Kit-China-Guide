87c302414615f05113fa6d9c7d1e02331cc5309a
Enrique Crespo <jecrespom@gmail.com>
2018-10-12 17:16:43 +0200

    Ejercicio 03

--- 02-Joystick/Analog_Joystick/Analog_Joystick.ino
+++ 02-Joystick/Analog_Joystick/Analog_Joystick.ino
@@ -7,18 +7,17 @@ const int Y_pin = 1; // analog pin connected to Y output
 void setup() {
   pinMode(SW_pin, INPUT);
   digitalWrite(SW_pin, HIGH);
-  Serial.begin(9600);
+  Serial.begin(250000);
 }
 
 void loop() {
   Serial.print("Switch:  ");
-  Serial.print(digitalRead(SW_pin));
-  Serial.print("\n");
+  Serial.print(digitalRead(SW_pin)*500);
+  Serial.print(" ");
   Serial.print("X-axis: ");
   Serial.print(analogRead(X_pin));
-  Serial.print("\n");
+  Serial.print(" ");
   Serial.print("Y-axis: ");
   Serial.println(analogRead(Y_pin));
-  Serial.print("\n\n");
-  delay(500);
+  Serial.print("\n");
 }
