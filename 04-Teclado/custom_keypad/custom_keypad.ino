c34a4885f2b8218d082e117a7cec2017e4c01b61
Enrique Crespo <jecrespom@gmail.com>
2018-10-13 19:03:20 +0200

    Ejercicio 04

---
+++ 04-Teclado/custom_keypad/custom_keypad.ino
@@ -0,0 +1,39 @@
+
+/* @file CustomKeypad.pde
+  || @version 1.0
+  || @author Alexander Brevig
+  || @contact alexanderbrevig@gmail.com
+  ||
+  || @description
+  || | Demonstrates changing the keypad size and key values.
+  || #
+*/
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = { //número ASCII no caracteres, no son imprimibles. Solo como indices
+  {1, 2, 3, 4},
+  {5, 6, 7, 8},
+  {9, 10, 11, 12},
+  {13, 14, 15, 16}
+};
+String teclas [] = {"S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "S13", "S14", "S15", "S16"};
+byte rowPins[ROWS] = {6, 7, 8, 9}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  char customKey = customKeypad.getKey();
+
+  if (customKey) {
+    Serial.println(teclas[(int)customKey-1]);
+  }
+}
