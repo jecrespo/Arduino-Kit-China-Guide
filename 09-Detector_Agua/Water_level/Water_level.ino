996615d4185ba44cc539d4c6b748641d704f2572
Enrique Crespo <jecrespom@gmail.com>
2018-10-16 17:02:26 +0200

    Ejercicio 09

---
+++ 09-Detector_Agua/Water_level/Water_level.ino
@@ -0,0 +1,21 @@
+
+int adc_id = 0;
+int HistoryValue = 0;
+char printBuffer[128];
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+    int value = analogRead(adc_id); // get adc value
+
+    if(((HistoryValue>=value) && ((HistoryValue - value) > 10)) || ((HistoryValue<value) && ((value - HistoryValue) > 10)))
+    {
+      sprintf(printBuffer,"ADC%d level is %d\n",adc_id, value);
+      Serial.print(printBuffer);
+      HistoryValue = value;
+    }
+}
